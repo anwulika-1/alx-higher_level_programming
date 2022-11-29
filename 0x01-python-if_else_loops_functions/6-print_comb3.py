bedeb4e35c168733c8d8bd30ac4938cc3f8f4408
anwulika-1 <anwulikahappinex@gmail.com>
2022-11-29 15:14:35 +0000

    Task

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,16 +1,5 @@
-#include <stdio.h>
-/**
- *main - entry point
- *Description: random number in n +ve/-ve
- *Return: 0 on success
- */
-int main(void)
-{
-int num, i;
-for (num = 48, i = 0; i < 10; num++, i++)
-{
-putchar(num);
-}
-putchar(10);
-return (0);
-}
+#!/usr/bin/python3
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
