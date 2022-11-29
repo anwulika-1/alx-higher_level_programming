05274f497bf76621fe8ab5fd1dd7e2587ce01fd9
anwulika-1 <anwulikahappinex@gmail.com>
2022-11-29 15:07:05 +0000

    Task

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,18 +1,3 @@
-#include <stdio.h>
-/**
- *main - entry point
- *Description: random number in n +ve/-ve
- *Return: 0 on success
- */
-int main(void)
-{
-int firstLetter, n;
-for (firstLetter = 97, n = 1; n < 27; firstLetter++, n++)
-{
-if (firstLetter == 101 || firstLetter == 113)
-continue;
-putchar(firstLetter);
-}
-putchar(10);
-return (0);
-}
+#!/usr/bin/python3
+for i in range(0, 99):
+    print("{} =".format(i), hex(i))
