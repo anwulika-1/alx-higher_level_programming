a8824041e14117583ce0ff9c263e351f010176de
anwulika-1 <anwulikahappinex@gmail.com>
2022-11-29 15:22:13 +0000

    Task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,20 +1,7 @@
-#include <stdio.h>
-/**
- *main - entry point
- *Description: random number in n +ve/-ve
- *Return: 0 on success
- */
-int main(void)
-{
-int num, hexletter, n;
-for (num = 48, n = 0; n < 10; num++, n++)
-{
-putchar(num);
-}
-for (hexletter = 97, n = 1; n < 7; hexletter++, n++)
-{
-putchar(hexletter);
-}
-putchar(10);
-return (0);
-}
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
